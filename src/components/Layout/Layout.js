776270cfb6dbce700eae0266aa1ce950c9a224a6
KoshiroKun <45243062+KoshiroKun@users.noreply.github.com>
2019-10-09 00:16:02 +0200

    navigation menu

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,10 @@
+import React from "react";
+import NavBar from '../NavBar/NavBar';
+
+const Layout = () => (
+    <div>
+        <NavBar />
+    </div>
+);
+
+export default Layout;
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,39 @@
+import React from "react";
+import Fab from "@material-ui/core/Fab";
+import Button from "@material-ui/core/Button";
+import HomeIcon from "@material-ui/icons/Home";
+import GetAppIcon from "@material-ui/icons/GetApp";
+import PolicyIcon from "@material-ui/icons/Policy";
+import ContactMailIcon from "@material-ui/icons/ContactMail";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-end;
+  flex-wrap: nowrap;
+  padding: 15px;
+
+  button {
+    margin: 0px 5px;
+  }
+`;
+
+const NavBar = () => (
+    <Wrapper>
+        <Fab color="secondary" aria-label="home">
+            <HomeIcon />
+        </Fab>
+        <Fab color="secondary" aria-label="getapp">
+            <GetAppIcon />
+        </Fab>
+        <Fab color="secondary" aria-label="policy">
+            <PolicyIcon />
+        </Fab>
+        <Fab color="secondary" aria-label="contact">
+            <ContactMailIcon />
+        </Fab>
+    </Wrapper>
+);
+
+export default NavBar;
